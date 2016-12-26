5e7ee67b8aca63f6252f012cd2104abce12013ff
Chicharlito <r-c95@hotmail.fr>
2016-12-26 17:17:04 +0100

    Changement du head qui est désormais included

--- index.php
+++ index.php
@@ -1,32 +1,8 @@
 <!DOCTYPE html>
 <!--[if IE 9 ]><html class="ie ie9" lang="en"> <![endif]-->
 <head>
-    
-    <!-- ================= Favicon ================== -->
-        <!-- Standard -->
-        <link rel="shortcut icon" type="images/png" href="images/full_1.png" />
-    
-<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
-
-<!-- Basic Page Needs
-================================================== -->
-<meta charset="utf-8">
-<title>FullBloom</title>
-<meta name="description" content="Solution de Platform As A Service." />
-
-<!-- Mobile Specific Metas
-================================================== -->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-<!-- CSS
-================================================== -->
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/colors/blue.css" id="colors">
-
-<!--[if lt IE 9]>
-	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-
+	<title>FullBloom</title>
+	<?php require_once 'inc/head.html';?>
 </head>
 
 <body>
